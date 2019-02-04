e278227191bc57b1c05c37c6efa4376881929fcc
Kacper Sagnowski <ksagnowski@gmail.com>
2019-02-04 12:09:42 +0000

    Add getSineSig.m

---
+++ getSineSig.m
@@ -0,0 +1,14 @@
+function sine = getSineSig(freq, duration, fs)
+    %GETSINESIG Generate a sine wave of given frequency and length
+    %   sine = GETSINESIG(freq, duration) uses default fs = 44100
+    % 
+    %   sine = GETSINESIG(freq, duration, fs) returns sine wave at a
+    %   specifed sampling frequency
+
+    if nargin == 2
+        fs = 44100;
+    end
+
+    n = 1:1:fs * duration;
+    sine = sin(2 * pi * n * freq / fs);
+end
