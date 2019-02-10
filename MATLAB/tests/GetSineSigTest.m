7b4eedbbc3bcee61aa468be98456ecd04503ee47
Kacper Sagnowski <ksagnowski@gmail.com>
2019-02-10 16:23:29 +0000

    Add phase argument to getSineSig.m

--- MATLAB/tests/GetSineSigTest.m
+++ MATLAB/tests/GetSineSigTest.m
@@ -2,14 +2,42 @@ classdef GetSineSigTest < matlab.unittest.TestCase
 
     methods (Test)
 
-        function testSine(testCase)
+        function testLongSineZeroPhase(testCase)
             freq = 440;
             len = 1000;
             fs = 44100;
 
             actual = getSineSig(freq, len);
-            expected = sin(2 * pi * (1:len)' * freq / fs);
+            expected = sin(2 * pi * ((1:len)' - 1) * freq / fs);
             testCase.verifyEqual(actual, expected);
+        end
+
+        function testShortSineZeroPhase(testCase)
+            actual = getSineSig(1, 4, 0, 4);
+            expected = [0; 1; 0; -1];
+
+            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
+        end
+
+        function testShortSineHalfPiPhase(testCase)
+            actual = getSineSig(1, 4, 0.5 * pi, 4);
+            expected = [1; 0; -1; 0];
+
+            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
+        end
+
+        function testShortSinePiPhase(testCase)
+            actual = getSineSig(1, 4, pi, 4);
+            expected = [0; -1; 0; 1];
+
+            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
+        end
+
+        function testShortSineMinusHalfPiPhase(testCase)
+            actual = getSineSig(1, 4, -0.5 * pi, 4);
+            expected = [-1; 0; 1; 0];
+
+            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
         end
 
     end
--- MATLAB/util/getSineSig.m
+++ MATLAB/util/getSineSig.m
@@ -1,14 +1,21 @@
-function sine = getSineSig(freq, len, fs)
+function sine = getSineSig(freq, len, phase, fs)
     %GETSINESIG Generate a sine wave of given frequency and length in samples
-    %   sine = GETSINESIG(freq, len) uses default fs = 44100
+    %   sine = GETSINESIG(freq, len, phase, fs) returns sine wave of
+    %   of frequency freq, with specified initial phase and at a specifed
+    %   sampling frequency fs. 
     % 
-    %   sine = GETSINESIG(freq, len, fs) returns sine wave at a
-    %   specifed sampling frequency
+    %   sine = GETSINESIG(freq, len, phase) uses default fs = 44100
+    % 
+    %   sine = GETSINESIG(freq, len) uses default fs = 44100 and phase = 0
 
-    if nargin == 2
+    if nargin < 4
         fs = 44100;
     end
 
+    if nargin < 3
+        phase = 0;
+    end
+
     n = (1:len)';
-    sine = sin(2 * pi * n * freq / fs);
+    sine = sin(2 * pi * (n - 1) * freq / fs + phase);
 end
