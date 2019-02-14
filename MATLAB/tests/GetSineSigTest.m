b4c980295b8ef2af5256dd1efefb048a7fef82cf
Kacper Sagnowski <ksagnowski@gmail.com>
2019-02-14 23:10:03 +0000

    Extend getSineSig to varying freq and amp

--- MATLAB/tests/GetSineSigTest.m
+++ MATLAB/tests/GetSineSigTest.m
@@ -9,35 +9,75 @@ classdef GetSineSigTest < matlab.unittest.TestCase
 
             actual = getSineSig(freq, len);
             expected = sin(2 * pi * ((1:len)' - 1) * freq / fs);
-            testCase.verifyEqual(actual, expected);
+            testCase.verifyEqual(actual, expected, 'AbsTol', 1.0e-13);
         end
 
         function testShortSineZeroPhase(testCase)
-            actual = getSineSig(1, 4, 0, 4);
+            actual = getSineSig(1, 4, 1, 0, 4);
             expected = [0; 1; 0; -1];
 
-            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
+            testCase.verifyEqual(actual, expected, 'AbsTol', 1.0e-13);
         end
 
         function testShortSineHalfPiPhase(testCase)
-            actual = getSineSig(1, 4, 0.5 * pi, 4);
+            actual = getSineSig(1, 4, 1, 0.5 * pi, 4);
             expected = [1; 0; -1; 0];
 
-            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
+            testCase.verifyEqual(actual, expected, 'AbsTol', 1.0e-13);
         end
 
         function testShortSinePiPhase(testCase)
-            actual = getSineSig(1, 4, pi, 4);
+            actual = getSineSig(1, 4, 1, pi, 4);
             expected = [0; -1; 0; 1];
 
-            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
+            testCase.verifyEqual(actual, expected, 'AbsTol', 1.0e-13);
         end
 
         function testShortSineMinusHalfPiPhase(testCase)
-            actual = getSineSig(1, 4, -0.5 * pi, 4);
+            actual = getSineSig(1, 4, 1, -0.5 * pi, 4);
             expected = [-1; 0; 1; 0];
 
-            testCase.verifyEqual(actual, expected, 'AbsTol', 0.00001);
+            testCase.verifyEqual(actual, expected, 'AbsTol', 1.0e-13);
+        end
+
+        function testVectorFreqConst(testCase)
+            fs = 44100;
+            len = 1000;
+            freq = 440;
+            actual = getSineSig(ones(len, 1) * freq, len, 1, 0);
+            expected = sin(2 * pi * (1:len)' * freq / fs);
+
+            testCase.verifyEqual(actual, expected, 'AbsTol', 1.0e-13);
+        end
+
+        function testVectorFreq(testCase)
+            fs = 44100;
+            len = 1000;
+            f0 = 100;
+            f1 = 200;
+            freq = linspace(f0, f1, len)';
+            phase = 0.5 * pi - 2 * pi * f0 / fs;
+            actual = getSineSig(freq, len, 1, phase);
+            t = linspace(0, len / fs, len)';
+            expected = chirp(t, f0, len / fs, f1);
+
+            err = expected - actual;
+            disp(['max abs error:', num2str(max(abs(err)))]);
+
+            testCase.verifyEqual(actual, expected, 'AbsTol', 0.02);
+        end
+
+        function testVectorAmp(testCase)
+            fs = 44100;
+            len = 1000;
+            freq = 440;
+            amp = linspace(0.1, 1, len)';
+            actual = getSineSig(freq, len, amp, 0);
+
+            indx = (1:len)';
+            expected = amp(indx) .* sin(2 * pi * (indx - 1) * freq / fs);
+
+            testCase.verifyEqual(actual, expected, 'AbsTol', 1.0e-13);
         end
 
     end
--- MATLAB/util/getSineSig.m
+++ MATLAB/util/getSineSig.m
@@ -1,21 +1,71 @@
-function sine = getSineSig(freq, len, phase, fs)
-    %GETSINESIG Generate a sine wave of given frequency and length in samples
-    %   sine = GETSINESIG(freq, len, phase, fs) returns sine wave of
-    %   of frequency freq, with specified initial phase and at a specifed
-    %   sampling frequency fs. 
-    % 
-    %   sine = GETSINESIG(freq, len, phase) uses default fs = 44100
-    % 
-    %   sine = GETSINESIG(freq, len) uses default fs = 44100 and phase = 0
+function sig = getSineSig(freq, len, amp, phase, fs)
+    %GETSINESIG Generate a sinewave of given frequency and length in samples
+    %   sig = GETSINESIG(freq, len, amp, phase, fs) returns sine wave of
+    %   frequency freq, amplitude amp, with specified initial phase and
+    %   at a specifed sampling frequency fs. Arguments freq and amp can
+    %   either be scalars or vectors of length len, mapping the value at
+    %   each index to the frequency or amplitude at corresponding sample of
+    %   the generated signal. If freq is given as a vector, the phase
+    %   argument is interpreted as the phase at index 0 (the sample
+    %   preceeding the generated signal).
+    %
+    %   sig = GETSINESIG(freq, len, amp, phase) uses default fs = 44100.
+    %
+    %   sig = GETSINESIG(freq, len, amp) uses default values: phase = 0
+    %   and fs = 44100.
+    %
+    %   sig = GETSINESIG(freq, len) uses default values: amp = 1, phase = 0
+    %   and fs = 44100.
 
-    if nargin < 4
+    if nargin < 5
         fs = 44100;
     end
 
-    if nargin < 3
+    if nargin < 4
         phase = 0;
     end
 
-    n = (1:len)';
-    sine = sin(2 * pi * (n - 1) * freq / fs + phase);
+    if nargin < 3
+        amp = 1;
+    end
+
+    if length(freq) ~= 1 && length(freq) ~= len
+        error('Frequency freq must be a scalar or a vector of length len!');
+    end
+
+    if length(amp) ~= 1 && length(amp) ~= len
+        error('Amplitude amp must be a scalar or a vector of length len!');
+    end
+
+    % If freq is a scalar, convert to a vector.
+    if length(freq) == 1
+        freq = ones(len, 1) * freq;
+        skipFirstPhase = false;
+    else
+        % If freq is a vector, treat the phase argument as the phase of the
+        % preceeding sample.
+        skipFirstPhase = true;
+    end
+
+    % If amp is a scalar, convert to a vector.
+    if length(amp) == 1
+        amp = ones(len, 1) * amp;
+    end
+
+    % Initialise
+    sig = zeros(len, 1);
+    n = 1;
+
+    % Generate signal
+    while n <= len
+
+        if skipFirstPhase
+            phase = getNextPhase(phase, freq(n), fs);
+        end
+
+        sig(n) = amp(n) * sin(phase);
+        n = n + 1;
+        skipFirstPhase = true;
+    end
+
 end
