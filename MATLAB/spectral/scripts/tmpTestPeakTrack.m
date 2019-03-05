a9b1b75da7613cce2d94dd02665d79b096466ae6
Kacper Sagnowski <ksagnowski@gmail.com>
2019-03-05 12:32:38 +0000

    Add macro for source sound in tmpTestPeakTrack.m

--- MATLAB/spectral/scripts/tmpTestPeakTrack.m
+++ MATLAB/spectral/scripts/tmpTestPeakTrack.m
@@ -1,22 +1,33 @@
 fs = 44100;
 frmLen = 1024;
 hopLen = 256;
-l = 200 * frmLen;
-f = [linspace(100, 2000, l).', linspace(1000, 3000, l).', ...
-        linspace(14000, 12000, l).'];
-a = [linspace(0.2, 1, l).', linspace(1, 0.5, l).', ...
-        0.5 + getCosSig(l, 1.3, 0.3)];
 
-s = getCosSig(l, f(:, 1), a(:, 1)) + ...
-    getCosSig(l, f(:, 2), a(:, 2)) + ...
-    getCosSig(l, f(:, 3), a(:, 3));
+% source = 'synth';
+source = 'flute';
 
-% s = audioread('audio/Flute.nonvib.ff.A4.wav');
+if strcmp(source, 'flute')
+    s = audioread('audio/Flute.nonvib.ff.A4.wav');
+else
+    l = 200 * frmLen;
+    f = [linspace(100, 2000, l).', linspace(1000, 3000, l).', ...
+            linspace(14000, 12000, l).'];
+    a = [linspace(0.2, 1, l).', linspace(1, 0.5, l).', ...
+            0.5 + getCosSig(l, 1.3, 0.3)];
+
+    s = getCosSig(l, f(:, 1), a(:, 1)) + ...
+        getCosSig(l, f(:, 2), a(:, 2)) + ...
+        getCosSig(l, f(:, 3), a(:, 3));
+end
+
 [freqEst, ampEst, phsEst, smpl] = trackSpecPeaks(s, frmLen, hopLen, 3);
 
-% plotPeakTrackingGT(f, freqEst, a, ampEst, smpl);
-% plotPeakTracking(freqEst, ampEst, smpl);
+if strcmp(source, 'flute')
+    plotPeakTracking(freqEst, ampEst, smpl);
+else
+    plotPeakTrackingGT(f, freqEst, a, ampEst, smpl);
+end
+
 initPhs = phsEst(1, :);
 s1 = resynth(freqEst, ampEst, initPhs, hopLen);
-sCropped = s(1+512:end-511);
+sCropped = s(1 + 512:end - 511);
 sDiff = sCropped - s1;
