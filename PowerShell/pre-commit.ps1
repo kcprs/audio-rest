c9257359240dc09a3f58876ad482195fad3f10ac
Kacper Sagnowski <ksagnowski@gmail.com>
2019-04-07 19:38:57 +0100

    Disable pre-commit testing script

--- PowerShell/pre-commit.ps1
+++ PowerShell/pre-commit.ps1
@@ -1,11 +1,11 @@
 # Run testing suite and save MATLAB exit code
-$exitCode = .\PowerShell\runTestSuite.ps1 -exit
+# $exitCode = .\PowerShell\runTestSuite.ps1 -exit
 
-# Allow or abort commit based on test results
-if ($exitCode -eq 100) {
-    Write-Host "Testing Successful." -ForegroundColor Green
-    exit 0
-} else {
-    Write-Host "Testing failed. Aborting commit." -ForegroundColor Red
-    exit -1
-}
+# # Allow or abort commit based on test results
+# if ($exitCode -eq 100) {
+#     Write-Host "Testing Successful." -ForegroundColor Green
+#     exit 0
+# } else {
+#     Write-Host "Testing failed. Aborting commit." -ForegroundColor Red
+#     exit -1
+# }
