2ceccc709ae69731c01efd60f4b88bf27fefeb69
Kacper Sagnowski <ksagnowski@gmail.com>
2019-04-27 22:04:53 +0100

    Add wfbar.m to submission folder

---
+++ MATLAB/submission/functions/wfbar.m
@@ -0,0 +1,24 @@
+function pred = wfbar(pre, post, gapLen, ord, rmDC)
+    %WFBAR Fix a gap in a signal using a weighted forward-backward predictor
+    %   pred = wfbar(pre, post, gapLen, ord, rmDC) returns prediction pred 
+    %   of length gapLen based on given pre- and post-gap sections using
+    %   an AR model of order ord.
+    % 
+    %   pred = wfbar(pre, post, gapLen, ord, rmDC) uses default value of
+    %   rmDC = false.
+
+    if nargin < 5
+        rmDC = false;
+    end
+
+    %% Restoration
+    % Predict the missing signal forward
+    predFwd = burgPredict(pre, ord, length(pre) + 1, gapLen, ...
+        length(pre), rmDc);
+
+    % Predict the missing signal backward
+    predBwd = burgPredict(post, ord, 0, -gapLen, length(post), rmDC);
+
+    % Apply the crossfade
+    pred = crossfade(predFwd, predBwd);
+end
--- MATLAB/submission/util/spgm.m
+++ MATLAB/submission/util/spgm.m
@@ -2,20 +2,20 @@ function spgm(sig, frmLen, hopLen, fs)
     %SPGM Plot a spectrogram using MATLAB's spectrogram function
 
     global fsGlobal;
-    
+
     if nargin < 4
         fs = fsGlobal;
     end
-    
+
     if nargin < 2
         frmLen = 2048;
     end
 
     if nargin < 3
-        hopLen = round(0.25 * frmLen);
+        hopLen = round(frmLen / 8);
     end
 
     overlap = frmLen - hopLen;
-        
+
     spectrogram(sig, hann(frmLen), overlap, frmLen, fs, 'yaxis');
 end
